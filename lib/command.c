d3fc7ebe8f4018c915874337ddfb35618903a11c
hasso <hasso>
2005-01-17 17:07:44 +0000

    * command.c (print_version): Do not bother even to examine host.name,           since it is always NULL when this function is called from main.
    	[backport from HEAD]

--- lib/command.c
+++ lib/command.c
@@ -142,10 +142,11 @@ level_match(const char *s)
   return ZLOG_DISABLED;
 }
 
+/* This is called from main when a daemon is invoked with -v or --version. */
 void
 print_version (const char *progname)
 {
-  printf ("%s version %s (%s)\n", progname, QUAGGA_VERSION, host.name);
+  printf ("%s version %s\n", progname, QUAGGA_VERSION);
   printf ("%s\n", QUAGGA_COPYRIGHT);
 }
 
