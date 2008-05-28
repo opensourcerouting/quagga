489d3935b90bc6f1a0b866f321c4d1fc9f480d8b
Stephen Hemminger <stephen.hemminger@vyatta.com>
2008-05-28 10:21:51 -0700

    fix compiler warnings
    Fix easy compiler warnings about unused code by marking them with #if 0
    Add one missing printf argument.

--- bgpd/bgp_clist.c
+++ bgpd/bgp_clist.c
@@ -836,6 +836,7 @@ community_list_init (void)
   return ch;
 }
 
+#if 0
 /* Terminate community-list.  */
 static void
 community_list_terminate (struct community_list_handler *ch)
@@ -857,3 +858,4 @@ community_list_terminate (struct community_list_handler *ch)
 
   XFREE (MTYPE_COMMUNITY_LIST_HANDLER, ch);
 }
+#endif
