5d07783dcb8914391af002aa4449f8bf1ae63228
Stephen Hemminger <stephen.hemminger@vyatta.com>
2008-07-28 12:19:04 -0700

    Make vtysh more useable for scripting
    Add environment variable (VTYSH_LOG) for logging.
    If a command fails, exit with non-zero exit code and don't
    continue multipart commands.

--- vtysh/vtysh.c
+++ vtysh/vtysh.c
@@ -271,10 +271,10 @@ vtysh_pager_init (void)
 }
 
 /* Command execution over the vty interface. */
-static void
+static int
 vtysh_execute_func (const char *line, int pager)
 {
-  int ret, cmd_stat;
+  int ret, cmd_stat = CMD_SUCCESS;
   u_int i;
   vector vline;
   struct cmd_element *cmd;
@@ -287,7 +287,7 @@ vtysh_execute_func (const char *line, int pager)
   vline = cmd_make_strvec (line);
 
   if (vline == NULL)
-    return;
+    return CMD_SUCCESS;
 
   saved_ret = ret = cmd_execute_command (vline, vty, &cmd, 1);
   saved_node = vty->node;
@@ -393,7 +393,7 @@ vtysh_execute_func (const char *line, int pager)
 			  }
 			fp = NULL;
 		      }
-		    return;
+		    return CMD_SUCCESS;
 		  }
 
 		ret = cmd_execute_command (vline, vty, &cmd, 1);
@@ -434,18 +434,19 @@ vtysh_execute_func (const char *line, int pager)
 	}
       fp = NULL;
     }
+  return cmd_stat;
 }
 
-void
+int
 vtysh_execute_no_pager (const char *line)
 {
-  vtysh_execute_func (line, 0);
+  return vtysh_execute_func (line, 0);
 }
 
-void
+int
 vtysh_execute (const char *line)
 {
-  vtysh_execute_func (line, 1);
+  return vtysh_execute_func (line, 1);
 }
 
 /* Configration make from file. */
