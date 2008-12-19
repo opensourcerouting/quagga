9e0c28740378b9cf12fafa1a3379eeca6497ce1d
Stephen Hemminger <stephen.hemminger@vyatta.com>
2008-12-19 15:12:33 -0800

    Backport jenner fixes for interface rename
    When interface is renamed, need to match with any old dead bits
    and notify daemons by delete/add.

--- zebra/interface.c
+++ zebra/interface.c
@@ -102,6 +102,7 @@ if_zebra_delete_hook (struct interface *ifp)
 	route_table_finish (zebra_if->ipv4_subnets);
 
       XFREE (MTYPE_TMP, zebra_if);
+      ifp->info = NULL;
     }
 
   return 0;
@@ -481,6 +482,40 @@ if_delete_update (struct interface *ifp)
      for setting ifindex to IFINDEX_INTERNAL after processing the
      interface deletion message. */
   ifp->ifindex = IFINDEX_INTERNAL;
+}
+
+/* Interfaces can only be renamed when DOWN */
+void
+if_rename (struct interface *ifp, const char *name)
+{
+	struct interface *oifp;
+
+	zebra_interface_delete_update (ifp);
+	listnode_delete (iflist, ifp);
+
+	/* rename overlaps earlier interface */
+	oifp = if_lookup_by_name(name);
+	if (oifp)
+	  {
+	      if (oifp->ifindex != IFINDEX_INTERNAL)
+		{
+		  zlog_err ("interface %s rename to %s overlaps with index %d",
+			    ifp->name, name, oifp->ifindex);
+		  if_delete_update (oifp);
+		}
+	      else if (IS_ZEBRA_DEBUG_KERNEL)
+		zlog_debug ("interface %s index %d superseded by rename of %s",
+			    oifp->name, oifp->ifindex, ifp->name);
+
+	      listnode_delete (iflist, oifp);
+	      XFREE (MTYPE_IF, oifp);
+	  }
+
+	strncpy(ifp->name, name, INTERFACE_NAMSIZ);
+	ifp->name[INTERFACE_NAMSIZ] = 0;
+	listnode_add_sort (iflist, ifp);
+
+	zebra_interface_add_update (ifp);
 }
 
 /* Interface is up. */
--- zebra/rt_netlink.c
+++ zebra/rt_netlink.c
@@ -466,13 +466,8 @@ netlink_interface (struct sockaddr_nl *snl, struct nlmsghdr *h)
     }
 
   /* Add interface. */
-  ifp = if_lookup_by_index(ifi->ifi_index);
-  if (!ifp)
-    {
-      ifp = if_create(name, strlen(name));
-      ifp->ifindex = ifi->ifi_index;
-    }
-  strncpy(ifp->name, name, INTERFACE_NAMSIZ);
+  ifp = if_get_by_name (name);
+  ifp->ifindex = ifi->ifi_index;
   ifp->flags = ifi->ifi_flags & 0x0000fffff;
   ifp->mtu6 = ifp->mtu = *(uint32_t *) RTA_DATA (tb[IFLA_MTU]);
   ifp->metric = 1;
@@ -977,7 +972,7 @@ netlink_link_change (struct sockaddr_nl *snl, struct nlmsghdr *h)
         {
           if (ifp == NULL)
 	    {
-	      ifp = if_create(name, strlen(name));
+	      ifp = if_get_by_name (name);
 	      ifp->ifindex = ifi->ifi_index;
 	      ifp->metric = 1;
 	    } 
@@ -995,24 +990,6 @@ netlink_link_change (struct sockaddr_nl *snl, struct nlmsghdr *h)
 	  /* If new link is added. */
 	  if_add_update (ifp);
         }
-      /* Interface name change */
-      else if (strcmp(ifp->name, name) != 0)
-	{
-	  ifp->mtu6 = ifp->mtu = mtu;
-
-	  zlog_info("interface index %d was renamed from %s to %s",
-		    ifi->ifi_index, ifp->name, name);
-
-	  if_delete_update(ifp);
-
-	  ifp = if_create(name, strlen(name));
-	  ifp->ifindex = ifi->ifi_index;
-	  ifp->metric = 1;
-	  ifp->mtu = ifp->mtu6 = mtu;
-	  ifp->flags = new_flags;
-
-	  if_add_update (ifp);
-	}
       /* Interface status change. */
       else if (new_flags != ifp->flags)
 	{
@@ -1036,6 +1013,15 @@ netlink_link_change (struct sockaddr_nl *snl, struct nlmsghdr *h)
 	      if (if_is_operative (ifp))
 		if_up (ifp);
 	    }
+	}
+      /* Interface name change */
+      else if (strcmp(ifp->name, name) != 0)
+	{
+	  ifp->mtu = ifp->mtu6 = mtu;
+	  zlog_info("interface index %d was renamed from %s to %s",
+		    ifi->ifi_index, ifp->name, name);
+
+	  if_rename (ifp, name);
 	}
       /* Interface mtu change */
       else if (mtu != ifp->mtu)
