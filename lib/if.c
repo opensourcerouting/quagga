a5fb7a05739d1232c30389ae75e751d572c6a26c
Stephen Hemminger <stephen.hemminger@vyatta.com>
2009-02-09 11:48:38 -0800

    zebra: change interface flag tests to inline
    Change the functions that test interface flags to inline, to reduce
    size and improve performance when rescan of link state on interface
    change.
    
       text	   data	    bss	    dec	    hex	filename
     166597	  17568	    640	 184805	  2d1e5	zebra/.libs/zebra (before)
     166143	  17528	    640	 184311	  2cff7	zebra/.libs/zebra (after)

--- lib/if.c
+++ lib/if.c
@@ -320,60 +320,6 @@ if_get_by_name_len(const char *name, size_t namelen)
 	 if_create(name, namelen);
 }
 
-/* Does interface up ? */
-int
-if_is_up (struct interface *ifp)
-{
-  return ifp->flags & IFF_UP;
-}
-
-/* Is interface running? */
-int
-if_is_running (struct interface *ifp)
-{
-  return ifp->flags & IFF_RUNNING;
-}
-
-/* Is the interface operative, eg. either UP & RUNNING
-   or UP & !ZEBRA_INTERFACE_LINK_DETECTION */
-int
-if_is_operative (struct interface *ifp)
-{
-  return ((ifp->flags & IFF_UP) &&
-	  (ifp->flags & IFF_RUNNING || !CHECK_FLAG(ifp->status, ZEBRA_INTERFACE_LINKDETECTION)));
-}
-
-/* Is this loopback interface ? */
-int
-if_is_loopback (struct interface *ifp)
-{
-  /* XXX: Do this better, eg what if IFF_WHATEVER means X on platform M
-   * but Y on platform N?
-   */
-  return (ifp->flags & (IFF_LOOPBACK|IFF_NOXMIT|IFF_VIRTUAL));
-}
-
-/* Does this interface support broadcast ? */
-int
-if_is_broadcast (struct interface *ifp)
-{
-  return ifp->flags & IFF_BROADCAST;
-}
-
-/* Does this interface support broadcast ? */
-int
-if_is_pointopoint (struct interface *ifp)
-{
-  return ifp->flags & IFF_POINTOPOINT;
-}
-
-/* Does this interface support multicast ? */
-int
-if_is_multicast (struct interface *ifp)
-{
-  return ifp->flags & IFF_MULTICAST;
-}
-
 /* Printout flag information into log */
 const char *
 if_flag_dump (unsigned long flag)
