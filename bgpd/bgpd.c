9fb3006d10c38f7337a705e82dd36bd3a963fa39
Stephen Hemminger <stephen.hemminger@vyatta.com>
2009-05-13 15:30:08 -0700

    Revert "Wait for peers to clear on BGP shutdown"
    This reverts commit 732ebcdfad6e3a786386e38a126c84477d192105.

--- bgpd/bgpd.c
+++ bgpd/bgpd.c
@@ -1133,8 +1133,6 @@ peer_delete (struct peer *peer)
   
   bgp_timer_set (peer); /* stops all timers for Deleted */
   
-  bgp_clear_route_wait (peer);
-
   /* Delete from all peer list. */
   if (! CHECK_FLAG (peer->sflags, PEER_STATUS_GROUP))
     {
