d041f26b8e5fafa1d2d0b7122d1d996a53caa84c
Denis Ovsienko <infrastation@yandex.ru>
2012-09-09 18:45:56 +0400

    zebra: fix struct rtadv memory type
    Add MTYPE_RTADV memory type and use it in rtadv_new() for the allocation
    of top-level struct rtadv. There is no deallocating function for the
    structure.

--- lib/memtypes.c
+++ lib/memtypes.c
@@ -75,6 +75,7 @@ struct memory_list memory_list_lib[] =
 
 struct memory_list memory_list_zebra[] = 
 {
+  { MTYPE_RTADV,		"Router Advertisement"		},
   { MTYPE_RTADV_PREFIX,		"Router Advertisement Prefix"	},
   { MTYPE_VRF,			"VRF"				},
   { MTYPE_VRF_NAME,		"VRF name"			},
