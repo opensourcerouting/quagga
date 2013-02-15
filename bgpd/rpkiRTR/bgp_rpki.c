6e938d14ddfb7098b1506a5c30d4a16e5a1f6768
Michael Mester <mester@zedat.fu-berlin.de>
2013-02-15 13:26:24 +0100

    Add first files

---
+++ bgpd/rpkiRTR/bgp_rpki.c
@@ -0,0 +1,8 @@
+/*
+ * bgp_rpki.c
+ *
+ *  Created on: 15.02.2013
+ *      Author: Michael Mester 
+ */
+
+
---
+++ bgpd/rpkiRTR/bgp_rpki.h
@@ -0,0 +1,13 @@
+/*
+ * bgp_rpki.h
+ *
+ *  Created on: 15.02.2013
+ *      Author: Michael Mester
+ */
+
+#ifndef BGP_RPKI_H_
+#define BGP_RPKI_H_
+
+
+
+#endif /* BGP_RPKI_H_ */
