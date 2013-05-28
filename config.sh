ff70c828323e569c57efe298c077610aae6e49f6
Mic <mic@linux-upps.site>
2013-05-28 18:15:23 +0200

    enable rpki in config.sh

--- config.sh
+++ config.sh
@@ -4,4 +4,4 @@
 # /bin/sh ./config.status --recheck
 ./configure  --prefix=/home/mic/Masterarbeit/Quagga-install/ \
 --localstatedir=/home/mic/Masterarbeit/Quagga-install/var \
---enable-user=mic --enable-group=users #--enable-rpki
+--enable-user=mic --enable-group=users --enable-rpki
