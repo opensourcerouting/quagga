cbecaffb45ea23f1adc2c0e4402c2cacd9cbcf53
Michael Mester <mester@zedat.fu-berlin.de>
2013-06-07 12:24:40 +0200

    rm config.sh for diff

--- config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-make -k distclean 
-/bin/sh ./config.status --recheck
-./configure  --prefix=/home/mic/Masterarbeit/Quagga-install/ \
---localstatedir=/home/mic/Masterarbeit/Quagga-install/var \
---enable-user=mic --enable-group=users --enable-rpki
