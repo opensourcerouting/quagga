ce505ceb68dce9db8ae8b363ea5fcbd08fe0794e
Dinesh G Dutt <ddutt@cumulusnetworks.com>
2014-09-30 15:06:23 -0700

    bgpd: bgpd-event-driven-route-map-updates.patch
    BGP: Reprocess the trigger points when an attached route map changes
    
    Currently, modifications to route maps do not affect already processed
    routes; they only affect new route updates. This patch addresses this
    limitation.
    
    Signed-off-by: Dinesh G Dutt <ddutt@cumulusnetworks.com>

--- bgpd/bgpd.c
+++ bgpd/bgpd.c
@@ -2335,6 +2335,9 @@ bgp_get (struct bgp **bgp_val, as_t *as, const char *name)
   bgp_router_id_set(bgp, &router_id_zebra);
   *bgp_val = bgp;
 
+  bgp->t_rmap_update = NULL;
+  bgp->rmap_update_timer = RMAP_DEFAULT_UPDATE_TIMER;
+
   /* Create BGP server socket, if first instance.  */
   if (list_isempty(bm->bgp)
       && !bgp_option_check (BGP_OPT_NO_LISTEN))
@@ -2358,6 +2361,9 @@ bgp_delete (struct bgp *bgp)
   struct listnode *next;
   afi_t afi;
   int i;
+
+  if (bgp->t_rmap_update)
+    BGP_TIMER_OFF(bgp->t_rmap_update);
 
   /* Delete static route. */
   bgp_static_delete (bgp);
@@ -4417,7 +4423,7 @@ peer_aslist_unset (struct peer *peer,afi_t afi, safi_t safi, int direct)
 }
 
 static void
-peer_aslist_update (void)
+peer_aslist_update (char *aslist_name)
 {
   afi_t afi;
   safi_t safi;
@@ -4468,6 +4474,40 @@ peer_aslist_update (void)
     }
 }
 
+static void
+peer_aslist_add (char *aslist_name)
+{
+  peer_aslist_update (aslist_name);
+  route_map_notify_dependencies((char *)aslist_name, RMAP_EVENT_ASLIST_ADDED);
+}
+
+static void
+peer_aslist_del (char *aslist_name)
+{
+  peer_aslist_update (aslist_name);
+  route_map_notify_dependencies((char *)aslist_name, RMAP_EVENT_ASLIST_DELETED);
+}
+
+static void
+peer_reprocess_routes (struct peer *peer, int direct,
+		       afi_t afi, safi_t safi)
+{
+  if (peer->status != Established)
+    return;
+
+  if (direct != RMAP_OUT)
+    {
+      if (CHECK_FLAG (peer->af_flags[afi][safi],
+		      PEER_FLAG_SOFT_RECONFIG))
+	bgp_soft_reconfig_in (peer, afi, safi);
+      else if (CHECK_FLAG (peer->cap, PEER_CAP_REFRESH_OLD_RCV)
+	       || CHECK_FLAG (peer->cap, PEER_CAP_REFRESH_NEW_RCV))
+	bgp_route_refresh_send (peer, afi, safi, 0, 0, 0);
+    }
+  else
+    bgp_announce_route(peer, afi, safi);
+}
+
 /* Set route-map to the peer. */
 int
 peer_route_map_set (struct peer *peer, afi_t afi, safi_t safi, int direct, 
@@ -4492,12 +4532,15 @@ peer_route_map_set (struct peer *peer, afi_t afi, safi_t safi, int direct,
 
   if (filter->map[direct].name)
     free (filter->map[direct].name);
-  
+
   filter->map[direct].name = strdup (name);
   filter->map[direct].map = route_map_lookup_by_name (name);
 
   if (! CHECK_FLAG (peer->sflags, PEER_STATUS_GROUP))
-    return 0;
+    {
+      peer_reprocess_routes(peer, direct, afi, safi);
+      return 0;
+    }
 
   group = peer->group;
   for (ALL_LIST_ELEMENTS (group->peer, node, nnode, peer))
@@ -4511,6 +4554,7 @@ peer_route_map_set (struct peer *peer, afi_t afi, safi_t safi, int direct,
 	free (filter->map[direct].name);
       filter->map[direct].name = strdup (name);
       filter->map[direct].map = route_map_lookup_by_name (name);
+      peer_reprocess_routes (peer, direct, afi, safi);
     }
   return 0;
 }
@@ -4558,7 +4602,10 @@ peer_route_map_unset (struct peer *peer, afi_t afi, safi_t safi, int direct)
   filter->map[direct].map = NULL;
 
   if (! CHECK_FLAG (peer->sflags, PEER_STATUS_GROUP))
-    return 0;
+    {
+      peer_reprocess_routes(peer, direct, afi, safi);
+      return 0;
+    }
 
   group = peer->group;
   for (ALL_LIST_ELEMENTS (group->peer, node, nnode, peer))
@@ -4572,6 +4619,7 @@ peer_route_map_unset (struct peer *peer, afi_t afi, safi_t safi, int direct)
 	free (filter->map[direct].name);
       filter->map[direct].name = NULL;
       filter->map[direct].map = NULL;
+      peer_reprocess_routes(peer, direct, afi, safi);
     }
   return 0;
 }
@@ -4600,7 +4648,10 @@ peer_unsuppress_map_set (struct peer *peer, afi_t afi, safi_t safi,
   filter->usmap.map = route_map_lookup_by_name (name);
 
   if (! CHECK_FLAG (peer->sflags, PEER_STATUS_GROUP))
-    return 0;
+    {
+      bgp_announce_route (peer, afi, safi);
+      return 0;
+    }
 
   group = peer->group;
   for (ALL_LIST_ELEMENTS (group->peer, node, nnode, peer))
@@ -4614,6 +4665,7 @@ peer_unsuppress_map_set (struct peer *peer, afi_t afi, safi_t safi,
 	free (filter->usmap.name);
       filter->usmap.name = strdup (name);
       filter->usmap.map = route_map_lookup_by_name (name);
+      bgp_announce_route (peer, afi, safi);
     }
   return 0;
 }
@@ -4640,7 +4692,10 @@ peer_unsuppress_map_unset (struct peer *peer, afi_t afi, safi_t safi)
   filter->usmap.map = NULL;
 
   if (! CHECK_FLAG (peer->sflags, PEER_STATUS_GROUP))
-    return 0;
+    {
+      bgp_announce_route(peer, afi, safi);
+      return 0;
+    }
 
   group = peer->group;
   for (ALL_LIST_ELEMENTS (group->peer, node, nnode, peer))
@@ -4654,6 +4709,7 @@ peer_unsuppress_map_unset (struct peer *peer, afi_t afi, safi_t safi)
 	free (filter->usmap.name);
       filter->usmap.name = NULL;
       filter->usmap.map = NULL;
+      bgp_announce_route(peer, afi, safi);
     }
   return 0;
 }
@@ -5732,6 +5788,10 @@ bgp_config_write (struct vty *vty)
 	vty_out (vty, " timers bgp %d %d%s", bgp->default_keepalive, 
 		 bgp->default_holdtime, VTY_NEWLINE);
 
+      if (bgp->rmap_update_timer != RMAP_DEFAULT_UPDATE_TIMER)
+	vty_out (vty, " bgp route-map delay-timer %d%s", bgp->rmap_update_timer,
+		 VTY_NEWLINE);
+
       /* peer-group */
       for (ALL_LIST_ELEMENTS (bgp->group, node, nnode, group))
 	{
@@ -5813,8 +5873,8 @@ bgp_init (void)
 
   /* Filter list initialize. */
   bgp_filter_init ();
-  as_list_add_hook (peer_aslist_update);
-  as_list_delete_hook (peer_aslist_update);
+  as_list_add_hook (peer_aslist_add);
+  as_list_delete_hook (peer_aslist_del);
 
   /* Prefix list initialize.*/
   prefix_list_init ();
