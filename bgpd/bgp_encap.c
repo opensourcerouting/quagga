0ae88eda1c48ea25e8cca71fc602956f7cfb7e7d
Lou Berger <lberger@labn.net>
2016-05-07 14:18:56 -0400

    bgpd: add L3/L2VPN Virtual Network Control feature
    This feature adds an L3 & L2 VPN application that makes use of the VPN
    and Encap SAFIs.  This code is currently used to support IETF NVO3 style
    operation.  In NVO3 terminology it provides the Network Virtualization
    Authority (NVA) and the ability to import/export IP prefixes and MAC
    addresses from Network Virtualization Edges (NVEs).  The code supports
    per-NVE tables.
    
    The NVE-NVA protocol used to communicate routing and Ethernet / Layer 2
    (L2) forwarding information between NVAs and NVEs is referred to as the
    Remote Forwarder Protocol (RFP). OpenFlow is an example RFP.  For
    general background on NVO3 and RFP concepts see [1].  For information on
    Openflow see [2].
    
    RFPs are integrated with BGP via the RF API contained in the new "rfapi"
    BGP sub-directory.  Currently, only a simple example RFP is included in
    Quagga. Developers may use this example as a starting point to integrate
    Quagga with an RFP of their choosing, e.g., OpenFlow.  The RFAPI code
    also supports the ability import/export of routing information between
    VNC and customer edge routers (CEs) operating within a virtual
    network. Import/export may take place between BGP views or to the
    default zebera VRF.
    
    BGP, with IP VPNs and Tunnel Encapsulation, is used to distribute VPN
    information between NVAs. BGP based IP VPN support is defined in
    RFC4364, BGP/MPLS IP Virtual Private Networks (VPNs), and RFC4659,
    BGP-MPLS IP Virtual Private Network (VPN) Extension for IPv6 VPN . Use
    of both the Encapsulation Subsequent Address Family Identifier (SAFI)
    and the Tunnel Encapsulation Attribute, RFC5512, The BGP Encapsulation
    Subsequent Address Family Identifier (SAFI) and the BGP Tunnel
    Encapsulation Attribute, are supported. MAC address distribution does
    not follow any standard BGB encoding, although it was inspired by the
    early IETF EVPN concepts.
    
    The feature is conditionally compiled and disabled by default.
    Use the --enable-bgp-vnc configure option to enable.
    
    The majority of this code was authored by G. Paul Ziemba
    <paulz@labn.net>.
    
    [1] http://tools.ietf.org/html/draft-ietf-nvo3-nve-nva-cp-req
    [2] https://www.opennetworking.org/sdn-resources/technical-library
    
    Note: includes changes to merge with ff-2016091901

--- bgpd/bgp_encap.c
+++ bgpd/bgp_encap.c
@@ -45,6 +45,10 @@ Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 #include "bgpd/bgp_vty.h"
 #include "bgpd/bgp_encap.h"
 
+#if ENABLE_BGP_VNC
+#include "bgpd/rfapi/rfapi_backend.h"
+#endif
+
 static void
 ecom2prd(struct ecommunity *ecom, struct prefix_rd *prd)
 {
@@ -184,7 +188,15 @@ bgp_nlri_parse_encap(
       if (attr) {
 	bgp_update (peer, &p, attr, afi, SAFI_ENCAP,
 		    ZEBRA_ROUTE_BGP, BGP_ROUTE_NORMAL, &prd, NULL, 0);
+#if ENABLE_BGP_VNC
+	rfapiProcessUpdate(peer, NULL, &p, &prd, attr, afi, SAFI_ENCAP,
+                           ZEBRA_ROUTE_BGP,  BGP_ROUTE_NORMAL, NULL);
+#endif
       } else {
+#if ENABLE_BGP_VNC
+	rfapiProcessWithdraw(peer, NULL, &p, &prd, attr, afi, SAFI_ENCAP,
+                             ZEBRA_ROUTE_BGP, 0);
+#endif
 	bgp_withdraw (peer, &p, attr, afi, SAFI_ENCAP,
 		      ZEBRA_ROUTE_BGP, BGP_ROUTE_NORMAL, &prd, NULL);
       }
--- bgpd/bgpd.h
+++ bgpd/bgpd.h
@@ -179,6 +179,11 @@ struct bgp
     u_int16_t maxpaths_ebgp;
     u_int16_t maxpaths_ibgp;
   } maxpaths[AFI_MAX][SAFI_MAX];
+
+#if ENABLE_BGP_VNC
+  struct rfapi_cfg *rfapi_cfg;
+  struct rfapi *rfapi;
+#endif
 };
 
 /* BGP peer-group support. */
@@ -228,6 +233,8 @@ struct bgp_rd
 #define RMAP_IMPORT   2
 #define RMAP_EXPORT   3
 #define RMAP_MAX        4
+
+#include "filter.h"
 
 /* BGP filter structure. */
 struct bgp_filter
@@ -401,6 +408,9 @@ struct peer
 #define PEER_FLAG_DISABLE_CONNECTED_CHECK   (1 << 6) /* disable-connected-check */
 #define PEER_FLAG_LOCAL_AS_NO_PREPEND       (1 << 7) /* local-as no-prepend */
 #define PEER_FLAG_LOCAL_AS_REPLACE_AS       (1 << 8) /* local-as no-prepend replace-as */
+#if ENABLE_BGP_VNC
+#define PEER_FLAG_IS_RFAPI_HD		    (1 << 9) /* attached to rfapi HD */
+#endif
 
   /* NSF mode (graceful restart) */
   u_char nsf[AFI_MAX][SAFI_MAX];
@@ -655,6 +665,9 @@ struct bgp_nlri
 #define BGP_ATTR_AS4_AGGREGATOR                 18
 #define BGP_ATTR_AS_PATHLIMIT                   21
 #define BGP_ATTR_ENCAP                          23
+#if ENABLE_BGP_VNC
+#define BGP_ATTR_VNC                           255
+#endif
 
 /* BGP update origin.  */
 #define BGP_ORIGIN_IGP                           0
@@ -767,6 +780,7 @@ struct bgp_nlri
 
 /* RFC4364 */
 #define SAFI_MPLS_LABELED_VPN                  128
+#define BGP_SAFI_VPN                           128
 
 /* Max TTL value.  */
 #define TTL_MAX                                255
@@ -998,4 +1012,8 @@ extern int peer_ttl_security_hops_set (struct peer *, int);
 extern int peer_ttl_security_hops_unset (struct peer *);
 
 extern void bgp_scan_finish (void);
+
+/* For benefit of rfapi */
+extern struct peer * peer_new (struct bgp *bgp);
+
 #endif /* _QUAGGA_BGPD_H */
